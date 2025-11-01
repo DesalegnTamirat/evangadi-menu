44f74474f3aa7f50ee2130deda0f3c24d95ab2de
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-11-01 18:22:28 +0300

    make FoodItem component dynamic

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,19 @@
-import { Component } from 'react'
-import styles from "./food.module.css"
+import { Component } from "react";
+import styles from "./food.module.css";
 
 export default class FoodItem extends Component {
   render() {
+    const { title, price, img, desc } = this.props;
     return (
       <div className={styles.singleFood}>
         <div>
-          <img src="https://www.willflyforfood.net/wp-content/uploads/2021/09/ethiopian-food-timatim-salata.jpg.webp" />
+          <img src={img} />
         </div>
         <div className={styles.titlePrice}>
-          <h3>TIMATIM SELAXA (ቲማቲም ሰላጣ)</h3>
-          <p>$5.99</p>
+          <h3>{title}</h3>
+          <p>${price}</p>
         </div>
-        <div className={styles.foodDesc}>
-          Timatim Salata refers to a type of fresh Ethiopian tomato salad that’s
-          also popular in Eritrea. It’s made with diced tomatoes, minced onions,
-          and finely chopped peppers dressed with a mixture of berbere spices,
-          olive oil, vinegar, and lemon juice.
-        </div>
+        <div className={styles.foodDesc}>{desc}</div>
       </div>
     );
   }
