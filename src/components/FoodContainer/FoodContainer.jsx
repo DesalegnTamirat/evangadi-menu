b1162f424bbdcf6a9125cea8e8e6016162f078bb
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-11-01 18:08:40 +0300

    add the food containers component to hold all menus together

---
+++ src/components/FoodContainer/FoodContainer.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import styles from "./foods.module.css";
+
+export default class FoodContainer extends Component {
+  render() {
+    return <div className={styles.foodsContainer}></div>;
+  }
+}
